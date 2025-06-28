24c671521c0b01fb0c750722ae71ea197535d0dd
dGrinderslev-dk <daniel.grinderslev1@gmail.com>
2025-06-28 16:18:34 +0200

    feat: tilføj lokal hosting af Google Fonts

--- resources/views/components/layouts/master.blade.php
+++ resources/views/components/layouts/master.blade.php
@@ -53,10 +53,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>{{ __($titleKey, $titleParams) ?? config('app.name', 'Laravel') }}</title>
 
-        {{-- Fonts --}}
+        {{-- Fonts (bruges ikke da de leveres lokalt) --}}
+        {{--
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+        --}}
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @stack('head') {{-- Akkumulerede stylesheets osv fra layout templates og pages --}}
