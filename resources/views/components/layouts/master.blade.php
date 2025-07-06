8c9fd76f900e38ac468ac335948d50fc2847a9df
dGrinderslev-dk <daniel.grinderslev1@gmail.com>
2025-07-07 00:14:08 +0200

    seo16

--- resources/views/components/layouts/master.blade.php
+++ resources/views/components/layouts/master.blade.php
@@ -123,7 +123,7 @@
 
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="apple-mobile-web-app-title" content="{{ config('app.name') }}">
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+        <meta name="apple-mobile-web-app-status-bar-style" content="default">
 
 
 
