927ec15bebaa581161ac9b5cce373d77c75e4f9c
dGrinderslev-dk <daniel.grinderslev1@gmail.com>
2025-07-06 23:59:37 +0200

    seo14

--- resources/views/components/layouts/master.blade.php
+++ resources/views/components/layouts/master.blade.php
@@ -122,7 +122,8 @@
         <meta x-ref="msTileColor" name="msapplication-TileColor" content="{{ $currentThemeColor }}">
 
         <meta name="apple-mobile-web-app-capable" content="yes">
-
+        <meta name="apple-mobile-web-app-title" content="giraf app">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black">
 
 
 
