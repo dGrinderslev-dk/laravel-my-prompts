458ad00231127d7d023eefd50249fe0e94dcf9b5
dGrinderslev-dk <daniel.grinderslev1@gmail.com>
2025-07-08 00:25:05 +0200

    added favicons using realfavicongenerator.net

--- resources/views/components/layouts/master.blade.php
+++ resources/views/components/layouts/master.blade.php
@@ -125,8 +125,14 @@
         <meta name="mobile-web-app-capable" content="yes">
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="apple-mobile-web-app-status-bar-style" content="default">
-        <meta name="apple-mobile-web-app-title" content="{{ config('app.name') }}">
-        <meta name="application-name" content="{{ config('app.name') }}">
+
+        {{-- Favicon and Touch Icons --}}
+        <link rel="icon" type="image/png" href="{{ asset('images/favicons/favicon-96x96.png') }}" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="{{ asset('images/favicons/favicon.svg') }}" />
+        <link rel="shortcut icon" href="{{ asset('images/favicons/favicon.ico') }}" />
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/favicons/apple-touch-icon.png') }}" />
+        <meta name="apple-mobile-web-app-title" content="{{ config('app.name') }}" />
+        <link rel="manifest" href="{{ asset('images/favicons/site.webmanifest') }}" />
 
         {{-- Security headers --}}
         <meta name="referrer" content="strict-origin-when-cross-origin">
