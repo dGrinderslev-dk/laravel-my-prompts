99dcb592bf0d903501d9cef39ad3b8dcdd17bad6
dGrinderslev-dk <daniel.grinderslev1@gmail.com>
2025-07-08 16:54:45 +0200

    changed h1 badge styling to correct size and made seo changes

--- resources/views/components/layouts/master.blade.php
+++ resources/views/components/layouts/master.blade.php
@@ -137,12 +137,12 @@
         {{-- Security headers --}}
         <meta name="referrer" content="strict-origin-when-cross-origin">
 
-        {{-- Fonts (bruges ikke da de leveres lokalt) --}}
-        {{--
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
-        --}}
+        {{-- Fonts (bruges ikke i production da de leveres lokalt) --}}
+        @if (!app()->isProduction())
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+        @endif
 
         {{-- ////=== ÆNDRING START ===//// --}}
         @if ($isAuthLayout)
